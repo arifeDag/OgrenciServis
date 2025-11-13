958e812923b293baca29e1c85b0c2e666f3a2738
Arife Kara <arifedagkara@gmail.com>
2025-11-13 23:25:17 +0300

    CRUD ögretmenler yapıldı.

--- OgrenciServis.Api/Controllers/OgretmenController.cs
+++ OgrenciServis.Api/Controllers/OgretmenController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OgrenciServis.Logic.Interface;
+using OgrenciServis.Models.DTO;
+using OgrenciServis.Models;
 
 namespace OgrenciServis.Api.Controllers
 {
@@ -15,10 +17,74 @@ namespace OgrenciServis.Api.Controllers
             this.ogretmen = ogretmen;
         }
         [HttpGet]
-
-        public ActionResult GetOgretmenler()
+        public ActionResult<OgretmenDto> GetOgretmenler()
         {
             return Ok(this.ogretmen.TumOgretmenleriListele());
         }
+        //get:api
+        [HttpGet("{id}")]
+
+        public ActionResult<OgretmenDto> GetOgretmen(int id)
+        {
+            var ogretmenDto = this.ogretmen.OgretmenGetirById(id);
+            if (ogretmenDto == null)
+            {
+                return NotFound($"ögretmen ID {id} bulunamadı.");
+            }
+            return Ok(ogretmenDto);
+        }
+        //post:api/ogretmen
+        [HttpPost]
+
+        public ActionResult<Ogretmen> PostOgretmen([FromBody] Ogretmen ogretmen)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var yeniOgretmen = this.ogretmen.OgretmenEkle(ogretmen);
+            return CreatedAtAction(nameof(GetOgretmen), new { id = yeniOgretmen.OgretmenId }, yeniOgretmen);
+
+        }
+        // Put : api/Ogretmen/5
+        [HttpPut("{id}")]
+        public ActionResult<Ogretmen> PutOgrenci(int id, [FromBody] Ogretmen ogretmen)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var guncellenenOgretmen = this.ogretmen.OgretmenGuncelle(id, ogretmen);
+
+            if (guncellenenOgretmen == null)
+
+            {
+                return NotFound($"Ögretmen ID {id} bulunamdı.");
+            }
+
+            return Ok(guncellenenOgretmen);
+
+
+        }
+
+        //Delete
+
+        [HttpDelete("{id}")]
+
+        public ActionResult DeleteOgretmen(int id)
+        {
+            var silindi = this.ogretmen.OgretmenSil(id);
+
+            if (!silindi)
+            {
+                return NotFound($"Ögretmen ID {id} bulunamadı");
+            }
+            return NoContent();
+
+        }
+
+
     }
 }
--- OgrenciServis.Logic/Interface/IOgretmen.cs
+++ OgrenciServis.Logic/Interface/IOgretmen.cs
@@ -1,9 +1,18 @@
-﻿using OgrenciServis.Models.DTO;
+﻿using OgrenciServis.Models;
+using OgrenciServis.Models.DTO;
 
 namespace OgrenciServis.Logic.Interface
 {
     public interface IOgretmen
     {
         IEnumerable<OgretmenDto> TumOgretmenleriListele();
+
+        OgretmenDto? OgretmenGetirById(int id);
+
+        Ogretmen OgretmenEkle(Ogretmen ogretmen);
+
+        Ogretmen? OgretmenGuncelle(int id, Ogretmen ogretmen);
+
+        bool OgretmenSil(int id);
     }
 } 
